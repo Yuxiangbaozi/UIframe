3c8e84329ec2bc5ba1fe0ecfc94d52285cb122c4
Yuxiangbaozi <378928394@qq.com>
2019-06-02 23:52:16 +0800

    安装 parcel

---
+++ src/button.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<button class="g-button">hi</button>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="scss" scoped>
+	.g-button {
+		font-size: var(--font-size);
+		height: var(--button-height);
+		padding: 0 1em;
+		border-radius: var(--border-radius);
+		border: 1px solid var(--border-color);
+		background: var(--button-bg);
+	}
+
+	.g-button:hover {
+		border-color: var(--border-color-hover);
+	}
+
+	.g-button:active {
+		background: var(--button-active-bg);
+	}
+
+	.g-button:focus {
+		outline: none;
+	}
+</style>
