e0268cb61e360c23df4f5b9253e65e8b94434e2b
Yuxiangbaozi <378928394@qq.com>
2019-07-03 22:48:00 +0800

    添加 sider 组件自由折叠展开功能

--- src/layout.vue
+++ src/layout.vue
@@ -1,38 +1,97 @@
 <template>
-	<div class="layout" :class="layClass">
-		<slot></slot>
+	<div>
+		<div class="layout" :class="layClass" v-if="xxx">
+			<slot></slot>
+			<g-button class="open" icon="right" @click="clickButton" v-show="yyy"></g-button>
+		</div>
+		<div class="layout" :class="layClass" v-else>
+			<slot></slot>
+		</div>
 	</div>
 </template>
 
 <script>
+	import Vue from 'vue'
+	import Button from './button'
+	import Header from './header'
+	import Content from './content'
+	import Footer from './footer'
+	import Sider from './sider'
 	export default {
 		name: "g-layout",
 		data(){
 			return {
 				layClass: {
 					hasSider: false,
-				}
+				},
+				xxx: false,
+				yyy: false,
+				eventBus: new Vue(),
+			}
+		},
+		provide(){
+			return {
+				eventBus: this.eventBus
+			}
+		},
+		methods: {
+			clickButton(){
+				this.yyy = false
+				this.eventBus.$emit('update:send', this.yyy)
 			}
 		},
 		mounted() {
 			this.$children.forEach((child)=>{
 				if (child.$options.name === 'g-sider') {
 					this.layClass.hasSider = true
+					if (child.$options.name === 'g-layout') {
+						this.xxx = false
+					}else {
+						this.xxx = true
+					}
 				}
 			})
-		}
+			this.eventBus.$emit('update:send',this.yyy)
+			this.eventBus.$on('update:change',(value)=> {
+				if (value === false){
+					setTimeout(()=> {
+						this.yyy = true
+					},500)
+				}
+			})
+		},
+		components: {
+			'g-button': Button,
+			'g-header': Header,
+			'g-content': Content,
+			'g-footer': Footer,
+			'g-sider': Sider,
+		},
+		
 	}
 </script>
 
 <style scoped lang="scss">
+	.g-button{
+		padding: 0;
+		width: 12px;
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+	}
 	.layout{
 		display:flex;
 		flex-direction: column;
 		flex-grow: 1;
-		border: 1px solid darkred;
+		border: 1px solid #888;
 		&.hasSider{
 			dispaly:flex;
 			flex-direction: row;
+		}
+		& .open{
+			position: absolute;
+			left: 0;
+			buttom: 50%;
+			transform: translateY(36px);
 		}
 	}
 </style>
--- src/sider.vue
+++ src/sider.vue
@@ -1,33 +1,55 @@
 <template>
 	<transition name="slide">
-		<div class="sider" v-if="visible">
+		<div class="sider" v-show="visible">
 			<slot></slot>
-			<button @click="visible=false">X</button>
+			<g-button class="close" icon="left" @click="onClick"></g-button>
 		</div>
 	</transition>
 </template>
 
 <script>
+	import Button from './button'
 	export default {
 		name: "g-sider",
+		inject: ['eventBus'],
 		data(){
 			return {
 				visible: true
 			}
 		},
+		components: {
+			'g-button': Button,
+		},
 		methods: {
-
+			onClick(){
+				this.visible = false
+				this.eventBus.$emit('update:change',this.visible)
+			}
+		},
+		mounted() {
+			this.eventBus.$on('update:send',(value)=> {
+				if (value === false){
+					this.visible = true
+				}
+			})
 		}
 	}
 </script>
 
 <style scoped lang="scss">
+	.g-button{
+		padding: 0;
+		width: 12px;
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+	}
 	.sider{
 		position: relative;
-		>button{
+		& .close{
 			position: absolute;
-			right: 0;
-			top: 0;
+			right: -12px;
+			buttom: 50%;
+			transform: translateY(36px);
 		}
 	}
 	.slide-enter-active, .slide-leave-active {
@@ -36,4 +58,5 @@
 	.slide-enter, .slide-leave-to {
 		transform: translateX(-100%);
 	}
+	
 </style>
